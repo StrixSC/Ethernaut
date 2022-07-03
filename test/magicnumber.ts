4acfaae1edab5e39226eba39c694bfe7775194bd
StrixSC <talon.strix@gmail.com>
2022-07-03 17:57:13 -0400

    Added some comments to document

--- test/magicnumber.ts
+++ test/magicnumber.ts
@@ -1,13 +1,10 @@
-import { expect } from "chai";
-import { BigNumber, Contract, ContractFactory, providers, Signer } from "ethers";
-import { Interface, keccak256, toUtf8Bytes } from "ethers/lib/utils";
+import { Contract, ContractFactory, providers, Signer } from "ethers";
 import { ethers } from "hardhat";
-import Web3 from "web3";
 
 describe("MagicNumber", function () {
     let signer: Signer;
     let player: string;
-    let web3: Web3;
+   
     let challenge_instance_address = "0x09ACe17f255d46470640441988792517Cd893C5b";
     let challengeFactory: ContractFactory;
     let challenge: Contract;
@@ -21,26 +18,22 @@ describe("MagicNumber", function () {
     });
 
     it("Should solve the challenge", async () => {
-        // Get the four first bytes of the function signature hash
-        // of the whatIsTheMeaningOfLife function:
-        const funcSignature = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("whatIsTheMeaningOfLife()"))
-        // See: https://monokh.com/posts/ethereum-contract-creation-bytecode
         const bytecode = [
             // init bytecode:
-            `60 0A`, // PUSH1 10 (The length of our runtime code)
-            `60 0c`, // PUSH1 12 (The position of the runtime code in the transaction data)
-            '60 00', // PUSH1 00 (The destination in memory)
+            `60 0A`, // PUSH1 10 (runtime code length in bytes)
+            `60 0c`, // PUSH1 12 (Position of the runtime code in the data (in bytes))
+            '60 00', // PUSH1 00 (Where the runtime code should be stored in memory.)
             '39',    // CODECOPY
-            `60 0A`, // PUSH1 10 (The length of our runtime code)
-            `60 00`, // PUSH1 00 (The memory location holding our runtime code)
+            `60 0A`, // PUSH1 10 (runtime code length in bytes)
+            `60 00`, // PUSH1 00 (Where the runtime code is stored in memory)
             'F3', // RETURN
             // Runtime Bytecode:
             `60 2A`, // PUSH1 42 
             `60 00`, // PUSH1 00 
-            `52`, // MSTORE 
+            `52`,   // MSTORE (Store 0x2a at position 0 in memory) 
             `60 20`, // PUSH1 32
             `60 00`, // PUSH1 00
-            `F3` // Return
+            `F3` // Return (Returns 32 bytes at position 0 of memory. (meaning returns 0x00000000...02A))
         ]
 
         const data = "0x" + bytecode.join('').replace(/ /g, '');
